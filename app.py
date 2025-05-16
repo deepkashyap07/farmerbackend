0431b28c83a5e74efdea499707b415d28ab29cbd
deepanshu kumar <deepanshukumar250@gmail.com>
2025-05-17 01:48:33 +0530

    hport

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def predict():
         return jsonify({"error": "An error occurred during prediction"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000,host="0.0.0.0")
+    app.run(host=config.HOST, port=config.PORT, debug=True)
